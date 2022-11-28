9ae660dedd03e1654d08fa58f84f1ed4fc4d3ac7
adryyy10 <adriafigueresgarcia@gmail.com>
2022-11-28 08:25:22 +0000

    Get question endpoint

---
+++ src/Controller/Trivia/Question/GetQuestionController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Controller\Trivia\Question;
+
+use App\Entity\Question;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class GetQuestionController extends AbstractController
+{
+
+    /**
+     * @Route("/question/{id}", name="app_trivia_question")
+     * 
+     * @param ManagerRegistry $doctrine
+     * @param int $id
+     * 
+     * @return JsonReponse
+     */
+    public function getQuestion(ManagerRegistry $doctrine, int $id): Response
+    {
+        $question = $doctrine->getRepository(Question::class)->find($id);
+
+        if (!$question) {
+            throw $this->createNotFoundException(
+                'No question found for id '.$id
+            );
+        }
+
+        return new JsonResponse([
+            'id'        => $question->getId(),
+            'statement' => $question->getStatement(),
+            'category'  => $question->getType(),
+            'answers'   => $this->getAnswersByQuestion($question)
+        ]);
+    }
+
+        /**
+     * Get the answers by the Question
+     * 
+     * @param Question $question
+     * 
+     * @return array
+     */
+    private function getAnswersByQuestion(Question $question): array
+    {
+        $answers            = [];
+        $questionAnswers    = $question->getAnswers();
+
+        if (!empty($questionAnswers)) {
+            foreach ($questionAnswers as $answer) {
+                array_push($answers, [
+                    'id'        => $answer->getId(),
+                    'title'     => $answer->getTitle(),
+                    'isCorrect' => $answer->getIsCorrect()
+                ]);
+            }
+        }
+
+        return $answers;
+    }
+
+}
