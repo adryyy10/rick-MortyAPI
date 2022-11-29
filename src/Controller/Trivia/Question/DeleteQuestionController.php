3e1a588e32d7b0d446c70ee611b1a2ec33897bcb
adryyy10 <adriafigueresgarcia@gmail.com>
2022-11-29 17:39:14 +0000

    Remove question endpoint

---
+++ src/Controller/Trivia/Question/DeleteQuestionController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller\Trivia\Question;
+
+use App\Entity\Question;
+use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DeleteQuestionController extends AbstractController
+{
+
+    /**
+     * @Route("/question/{id}", methods={"DELETE"}, name="app_trivia_delete_question")
+     * 
+     * @param ManagerRegistry $doctrine
+     * @param int $id
+     * 
+     * @return JsonReponse
+     */
+    public function getQuestion(ManagerRegistry $doctrine, int $id): Response
+    {
+
+        /** We cannot update unless we are ROLE_SUPER_ADMIN */
+        $this->denyAccessUnlessGranted('ROLE_SUPER_ADMIN', null, 'User tried to add without having ROLE_SUPER_ADMIN');
+
+        $question = $doctrine->getRepository(Question::class)->find($id);
+
+        if (!$question) {
+            throw $this->createNotFoundException(
+                'No question found for id '.$id
+            );
+        }
+
+        $entityManager = $doctrine->getManager();
+        $entityManager->remove($question);
+        $entityManager->flush();
+
+        return new Response('Deleted question');
+    }
+
+}
